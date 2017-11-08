3af32712ae71d4fbc5d4403f750fffd40b487f34
Nick M. Mitchell <nickm@us.ibm.com>
2017-11-08 16:36:17 -0500

    add plugin preloader mechanism

---
+++ app/plugins/admin/preloader.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require('path'),
+      { preload } = require(path.join(__dirname, '../preload.json'))
+
+/**
+ * This plugin allows for plugins to register themselves to be
+ * preloaded at startup, rather than in response to a user command
+ *
+ */
+module.exports = (_, prequire) => {
+    eventBus.on('/window/init', () => {
+        //
+        // on init, use prequire to load each plugin that desires to
+        // be preloaded
+        //
+        preload.forEach(prequire)
+    })
+}
