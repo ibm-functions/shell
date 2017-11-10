5b9ae36aab8e853eba5eeac9ab5fd2ebdea09cea
Nick M. Mitchell <nickm@us.ibm.com>
2017-11-10 12:02:58 -0500

    removed some of the publish-to-objectstore and publish-to-npm scripts
    these are really provider-specific, not for open source

--- prebuilt/bin/uninstall.js
+++
@@ -1,7 +0,0 @@
-// npm tries to make a fsh.cmd for us. it doesn't work with our bat file.
-if (process.platform === 'win32') {
-    const cmd = require('path').join(process.cwd(), '..', '..', '..', 'fsh.cmd')
-    if (require('fs').existsSync(cmd)) {
-        require('fs').unlinkSync(cmd)
-    }
-}
