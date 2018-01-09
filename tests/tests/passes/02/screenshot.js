eeca8f2292f1ede9de3c86240c0cb8c8a4d01de3
Nick M. Mitchell <nickm@us.ibm.com>
2018-01-09 13:11:39 -0500

    add screenshot save to disk feature
    Fixes #207

--- tests/tests/passes/02/screenshot.js
+++ tests/tests/passes/02/screenshot.js
@@ -33,6 +33,7 @@ const takeScreenshot = function(ctx, which='') {
     return cli.do(`screenshot ${which}`, ctx.app)
         .then(res => ctx.app.client.waitForExist('#screenshot-captured')
               .then(() => ctx.app.client.waitForEnabled('#screenshot-captured .go-away-button'))
+              .then(() => ctx.app.client.click('#screenshot-captured .sidecar-bottom-stripe-save'))
               .then(() => ctx.app.client.click('#screenshot-captured .sidecar-bottom-stripe-close'))
               .then(() => ctx.app.client.waitForExist('#screenshot-captured', 5000, true)) // false meaning better not be visible
               .then(() => res)
