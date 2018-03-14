e41373a54aafceb548a64662ed05aac3d35c58a3
Nick M. Mitchell <nickm@us.ibm.com>
2018-03-13 21:54:20 -0400

    expand text-search test
    Fixes #650

--- tests/tests/passes/02/text-search.js
+++ tests/tests/passes/02/text-search.js
@@ -32,7 +32,7 @@ describe('Text search', function() {
        .catch(common.oops(this)))
 
     it('should not close the search bar if pressing esc outside of search input', () => this.app.client.click(ui.selectors.CURRENT_PROMPT_BLOCK)
-       .then(() => this.app.client.keys('\uE00C'))
+       .then(() => this.app.client.keys(keys.ESCAPE))
        .then(() => this.app.client.isVisible('#search-bar'))
        .then(r => assert.ok(r, 'assert if search-bar is visible'))
        .catch(common.oops(this)))
@@ -42,7 +42,7 @@ describe('Text search', function() {
        .then(r => assert.ok(r, 'assert if search-input is focused'))
        .catch(common.oops(this)))
 
-    it('should close the search bar if pressing esc in search input', () => this.app.client.setValue('#search-input', '\uE00C')
+    it('should close the search bar if pressing esc in search input', () => this.app.client.setValue('#search-input', keys.ESCAPE)
        .then(() => this.app.client.waitForVisible('#search-bar', 2000, false))
        .catch(common.oops(this)))
 
@@ -52,7 +52,58 @@ describe('Text search', function() {
        .then(r => assert.ok(r, 'search-bar visible'))
        .catch(common.oops(this)))
 
+    // 2 matches test
     it('should close the search bar if clicking the close button', () => this.app.client.click('#search-close-button')
        .then(() => this.app.client.waitForVisible('#search-bar', 2000, false))
+       .catch(common.oops(this)))
+    it('should add grumble to the repl', () => cli.do('grumble', this.app)
+       .then(cli.expectError(0))
+       .catch(common.oops(this)))
+    it('should add another grumble to the repl', () => cli.do('grumble', this.app)
+       .then(cli.expectError(0))
+       .catch(common.oops(this)))
+    it('should find 2 matches for grumble', () => this.app.client.keys([ui.ctrlOrMeta, 'f'])
+       .then(() => this.app.client.waitForVisible('#search-bar'))
+       .then(() => this.app.client.hasFocus('#search-input'))
+       .then(r => assert.ok(r, 'assert if search-input is focused'))
+       .then(() => this.app.client.waitUntil(() => {
+           return this.app.client.setValue('#search-input', `grumble${keys.ENTER}`)
+               .then(() => this.app.client.getText('#search-found-text'))
+               .then(txt => txt === '2 matches')
+       }))
+       .catch(common.oops(this)))
+
+    // 1 match test
+    it('should close the search bar if clicking the close button', () => this.app.client.click('#search-close-button')
+       .then(() => this.app.client.waitForVisible('#search-bar', 2000, false))
+       .catch(common.oops(this)))
+    it('should add bojangles to the repl', () => cli.do('bojangles', this.app)
+       .then(cli.expectError(0))
+       .catch(common.oops(this)))
+    it('should find 1 match for bojangles', () => this.app.client.keys([ui.ctrlOrMeta, 'f'])
+       .then(() => this.app.client.waitForVisible('#search-bar'))
+       .then(() => this.app.client.hasFocus('#search-input'))
+       .then(r => assert.ok(r, 'assert if search-input is focused'))
+       .then(() => this.app.client.waitUntil(() => {
+           return this.app.client.setValue('#search-input', `bojangles${keys.ENTER}`)
+               .then(() => this.app.client.getText('#search-found-text'))
+               .then(txt => txt === '1 match')
+       }))
+       .catch(common.oops(this)))
+
+    // no matches test
+    it('should close the search bar if clicking the close button', () => this.app.client.click('#search-close-button')
+       .then(() => this.app.client.waitForVisible('#search-bar', 2000, false))
+       .catch(common.oops(this)))
+    // re-open, so that we can test entering text and hitting enter
+    it('should find nothing when searching for waldo', () => this.app.client.keys([ui.ctrlOrMeta, 'f'])
+       .then(() => this.app.client.waitForVisible('#search-bar'))
+       .then(() => this.app.client.hasFocus('#search-input'))
+       .then(r => assert.ok(r, 'assert if search-input is focused'))
+       .then(() => this.app.client.waitUntil(() => {
+           return this.app.client.setValue('#search-input', `waldo${keys.ENTER}`)
+               .then(() => this.app.client.getText('#search-found-text'))
+               .then(txt => txt === 'no matches')
+       }))
        .catch(common.oops(this)))
 })
