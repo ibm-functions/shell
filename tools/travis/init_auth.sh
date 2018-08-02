ba62df05a62a4200d9b3704d08795db4af0b1f6d
Nick Mitchell <nickm@us.ibm.com>
2018-08-02 08:04:47 -0400

    split 04 test suite in two
    Fixes #1086

--- tools/travis/init_auth.sh
+++ tools/travis/init_auth.sh
@@ -25,7 +25,8 @@ cd $SCRIPTDIR
 ./wskadmin user delete user01  | grep -v "Failed to delete subject"
 ./wskadmin user delete user02  | grep -v "Failed to delete subject"
 ./wskadmin user delete user03  | grep -v "Failed to delete subject"
-./wskadmin user delete user04  | grep -v "Failed to delete subject"
+./wskadmin user delete user04a  | grep -v "Failed to delete subject"
+./wskadmin user delete user04ab  | grep -v "Failed to delete subject"
 ./wskadmin user delete user04b | grep -v "Failed to delete subject"
 ./wskadmin user delete user05  | grep -v "Failed to delete subject"
 ./wskadmin user delete user06  | grep -v "Failed to delete subject"
@@ -35,7 +36,8 @@ cd $SCRIPTDIR
 ./wskadmin user create user01 -ns ns01 > $DIR/01    2> /dev/null
 ./wskadmin user create user02 -ns ns02 > $DIR/02    2> /dev/null
 ./wskadmin user create user03 -ns ns03 > $DIR/03    2> /dev/null
-./wskadmin user create user04 -ns ns04 > $DIR/04    2> /dev/null
+./wskadmin user create user04a -ns ns04a > $DIR/04a    2> /dev/null
+./wskadmin user create user04ab -ns ns04ab > $DIR/04ab    2> /dev/null
 ./wskadmin user create user04b -ns ns04b > $DIR/04b 2> /dev/null
 ./wskadmin user create user05 -ns ns05 > $DIR/05    2> /dev/null
 ./wskadmin user create user06 -ns ns06 > $DIR/06    2> /dev/null
